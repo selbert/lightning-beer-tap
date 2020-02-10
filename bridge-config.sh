ce089cd5051bc681372b3d05ed51791530d40357
tselber <selber@puzzle.ch>
2020-02-10 15:10:28 +0100

    fixed export to have variable in application

--- bridge-config.sh
+++ bridge-config.sh
@@ -6,7 +6,7 @@ DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 # bridge config
 # ----------------------------------------------
 
-APP_BACKEND_URL="http://localhost:8080/api/invoice/subscribe"
+export APP_BACKEND_URL="http://localhost:8080/api/invoice/subscribe"
 
-APP_EXEC_PATH="${DIR}/gpio_handler/gpio_handler.py"
-APP_MEMO_PREFIX="beerTap"
+export APP_EXEC_PATH="${DIR}/gpio_handler/gpio_handler.py"
+export APP_MEMO_PREFIX="beerTap"
