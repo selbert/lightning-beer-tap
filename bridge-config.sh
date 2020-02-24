a682a02bd26672fbf592095af98781ca98321738
tselber <selber@puzzle.ch>
2020-02-24 15:30:48 +0100

    fixed wrong env var

--- bridge-config.sh
+++ bridge-config.sh
@@ -9,4 +9,4 @@ DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 export APP_BACKEND_URL="http://localhost:8080/api/invoice/subscribe"
 
 export APP_EXEC_PATH="${DIR}/gpio_handler/gpio_handler.py"
-export APP_MEMO_PREFIX="beerTap"
+export APP_BEERTAP_NAME="beerTap"
